a1db747507c312e7815a9030adbbc7b06345545d
S0007914288 <thomas.nelissen@flexso.com>
2018-07-19 11:17:39 +0000

    Initial commit of the icons library

---
+++ installscripts/script.js
@@ -0,0 +1,18 @@
+var child_process = require('child_process');
+var hostname = require("os").hostname();
+console.log("start postinstall script");
+console.log("Running on:" + hostname);
+if (hostname.indexOf("devxbuild") > -1) {
+	console.log("Don't install bower, running in SAP Web IDE CP");
+	return;
+}
+console.log("install bower");
+const commands = [
+	'bower install'
+];
+
+const executedCommands = child_process.exec(commands.join('&&'), (error) => {
+	if (error) {
+		throw error;
+	}
+});
